8ce623b8785ef1097479315a8e44d4bc805e7fc6
Cptmacmillan2022007 <97718560+Cptmacmillan2022007@users.noreply.github.com>
2022-06-02 13:43:20 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -10,14 +10,6 @@ install -m 755 /tmp/xray/geoip.dat /usr/local/bin/geoip.dat
 xray -version
 rm -rf /tmp/xray
 
-# Get CoreDNS and decompress binary
-mkdir /tmp/coredns
-curl --retry 10 --retry-max-time 60 -L -H "Cache-Control: no-cache" -fsSL github.com/coredns/coredns/releases/download/v1.9.3/coredns_1.9.3_linux_amd64.tgz -o /tmp/coredns/coredns.tgz
-tar -zxvf /tmp/coredns/coredns.tgz -C /tmp/coredns
-install -m 755 /tmp/coredns/coredns /usr/local/bin/coredns
-coredns -version
-rm -rf /tmp/coredns
-
 # V2/X2 new configuration
 install -d /usr/local/etc/xray
 cat << EOF > /usr/local/etc/xray/config.json
@@ -116,8 +108,8 @@ cat << EOF > /usr/local/etc/xray/config.json
     "dns": {
         "servers": [
             {
-                "address": "127.0.0.1",
-                "port": 5653,
+                "address": "https+local://dns.google/dns-query",
+                "address": "https+local://cloudflare-dns.com/dns-query",
                 "skipFallback": true
             }
         ],
@@ -128,19 +120,5 @@ cat << EOF > /usr/local/etc/xray/config.json
 }
 EOF
 
-# CoreDNS new configuration
-install -d /usr/local/etc/coredns
-cat << EOF > /usr/local/etc/coredns/config.json
-.:5653 {
-    bind 127.0.0.1
-    forward . tls://8.8.8.8 tls://8.8.4.4 {
-        tls_servername dns.google
-        force_tcp
-        health_check 5s
-    }
-    reload 10s
-}
-EOF
-
 # Run V2/X2
-/usr/local/bin/xray -config /usr/local/etc/xray/config.json & /usr/local/bin/coredns -conf /usr/local/etc/coredns/config.json
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json
